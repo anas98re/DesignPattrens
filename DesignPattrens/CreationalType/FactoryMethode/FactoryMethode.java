96aa02f06a88ac39af94b1637162821c77ea3a04
Anas Alrefai <101460051+anas98re@users.noreply.github.com>
2024-06-24 00:05:01 +0300

    Update FactoryMethode.java

--- DesignPattrens/CreationalType/FactoryMethode/FactoryMethode.java
+++ DesignPattrens/CreationalType/FactoryMethode/FactoryMethode.java
@@ -1,1 +1,78 @@
+interface Document {
+    void open();
+    void close();
+    void save();
+}
 
+//TextDocument class
+class TextDocument implements Document {
+    @Override
+    public void open(){
+        system.output.println("open");
+    }
+
+    @Override
+    public void close(){
+        system.output.println("close");
+    }
+
+    @Override
+    public void save(){
+        system.output.println("save");
+    }
+}
+
+//SpreadsheetDocument class 
+class SpreadsheetDocument implements Document {
+    @Override
+    public void open(){
+        system.output.println("open spreadsheet ");
+    }
+
+    @Override
+    public void close(){
+        system.output.println("close spreadsheet ");
+    }
+
+    @Override
+    public void save(){
+        system.output.println("save spreadsheet ");
+    }
+}
+
+//Application Abstract class
+abstract class Application {
+    abstract Document createDocument();
+
+    void newDocument(){
+        Document doc = createDocument();
+        doc.open();
+    }
+}
+
+//TextApplication class
+class TextApplication extends Application {
+    @Override
+    Document createDocument(){
+        return new TextDocument();
+    }
+}
+
+//SpreadsheetApplication class
+class SpreadsheetApplication extends Application{
+    @Override
+    Document createDocument(){
+        return new SpreadsheetDocument();
+    }
+}
+
+//Main class
+public class Main{
+    public static void main(String[] args){
+        Application App = new TextApplication();
+        app.newDocument();
+
+        Application AppSpreadsheet = new SpreadsheetApplication();
+        AppSpreadsheet.newDocument();
+    }
+}
